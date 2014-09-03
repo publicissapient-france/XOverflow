355e8a6aef21514f5daf803494e0bba142587f00
rbung <rkbung@gmail.com>
2014-09-03 10:40:18 +0200

    add static boiler5boilerplate

--- src/main/java/com/xebia/xoverflow/server/XoverflowServer.java
+++ src/main/java/com/xebia/xoverflow/server/XoverflowServer.java
@@ -5,6 +5,7 @@ import static spark.Spark.*;
 public class XoverflowServer {
 
     public static void main(String[] args) {
+        staticFileLocation("/");
         get("/hello",(request, response) -> "Hello World!");
     }
 
