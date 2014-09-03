4fe80c32e135f92e8965241a7a0b3a7f6e9f7c08
slemesle <slemesle@xebia.fr>
2014-09-03 10:43:40 +0200

    Fck

--- src/main/java/com/xebia/xoverflow/server/XoverflowServer.java
+++ src/main/java/com/xebia/xoverflow/server/XoverflowServer.java
@@ -4,9 +4,13 @@ import static spark.Spark.*;
 
 public class XoverflowServer {
 
+
     public static void main(String[] args) {
         staticFileLocation("/");
         get("/hello",(request, response) -> "Hello World!");
     }
+
+
+
 
 }
