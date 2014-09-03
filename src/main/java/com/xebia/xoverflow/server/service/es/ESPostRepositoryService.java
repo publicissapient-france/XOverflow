8149d1fcc86886926fdf50a240703f4afc450c4c
ArnaudPiroelle <piroelle.arnaud@gmail.com>
2014-09-03 10:55:35 +0200

    Add retrofit dependency and ES implementation

---
+++ src/main/java/com/xebia/xoverflow/server/service/es/ESPostRepositoryService.java
@@ -0,0 +1,10 @@
+package com.xebia.xoverflow.server.service.es;
+
+import com.xebia.xoverflow.server.service.PostRepositoryService;
+
+/**
+ * Created by arnaud on 03/09/2014.
+ */
+public class ESPostRepositoryService implements PostRepositoryService {
+
+}
