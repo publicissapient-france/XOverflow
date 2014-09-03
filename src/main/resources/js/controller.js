9da8055e8432c55f72d827a96e7d6bdf04e2b4da
rbung <rkbung@gmail.com>
2014-09-03 13:24:38 +0200

    list posts in home page

--- src/main/resources/js/controller.js
+++ src/main/resources/js/controller.js
@@ -1,11 +1,10 @@
+function extracted($http) {
+    return $http.get("/posts");
+}
+
 angular.module('XOverFlow', [])
     .controller('PostController', ['$scope', '$http', function($scope, $http) {
-        $scope.posts = [
-            {subject:'Question 1 ?'},
-            {subject:'Question 2 ?'},
-            {subject:'Question 3 ?'},
-            {subject:'Question 4 ?'},
-            {subject:'Question 5 ?'}];
+
 
         $scope.postquestion = function(question) {
             $http.put("/post", question);
@@ -16,6 +15,13 @@ angular.module('XOverFlow', [])
         $scope.readquestion = function(questionid) {
 //           return $http.get("/post" + questionid);
             return {subject:'Question', body:'toto', username:'username', date:'date'};
+        }
+
+        $scope.initposts = function() {
+            var response = $http.get("/posts");
+            response.success(function (data){
+                $scope.posts = data;
+            })
         }
 
     }]);
