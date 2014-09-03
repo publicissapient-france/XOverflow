d9b49831aa7d5fa1b62f8147ad5ec17a75d4cc3c
Antoine MICHAUD <antoinemichaud34@gmail.com>
2014-09-03 12:25:22 +0200

    send new question

--- src/main/resources/js/controller.js
+++ src/main/resources/js/controller.js
@@ -1,10 +1,14 @@
 angular.module('XOverFlow', [])
-    .controller('PostController', ['$scope', function($scope) {
+    .controller('PostController', ['$scope', '$http', function($scope, $http) {
         $scope.posts = [
             {subject:'Question 1 ?'},
             {subject:'Question 2 ?'},
             {subject:'Question 3 ?'},
             {subject:'Question 4 ?'},
             {subject:'Question 5 ?'}];
+
+        $scope.postquestion = function(question) {
+            $http.put("/post", question);
+        }
 
     }]);
